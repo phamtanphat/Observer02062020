d4b70e294b69119dfae2faec2be99145e61763dc
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-11 18:33:30 +0700

    function

--- app/src/main/java/com/example/observer02062020/MainActivity.java
+++ app/src/main/java/com/example/observer02062020/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.observer02062020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Button button = new Button(this);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+
+        inSoChinhPhuong();
+    }
+
+    // Chạy vòng lặp 1 - 100
+    // function inSoChan
+    // function inSoLe
+    // function inSoChia3Du1
+    // function inSoChinhPhuong (Căn của giá trị và phải là số nguyên)
+
+    private void inSoChan(){
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0){
+                Log.d("BBB",i + "");
+            }
+        }
+    }
+    private void inSoLe(){
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1){
+                Log.d("BBB",i + "");
+            }
+        }
+    }
+    private void inSoChia3Du1(){
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 1){
+                Log.d("BBB",i + "");
+            }
+        }
+    }
+    private void inSoChinhPhuong(){
+        for (int i = 1; i <= 100; i++) {
+            if (Math.sqrt(i) % 1 == 0){
+                Log.d("BBB",i + "");
+            }
+        }
     }
 }
